0e332dd4b7ed280d3bdaf5e4bd4404712f489a1d
sofiaramirezd <sofiaramirez@dugelby.com>
2026-03-12 13:07:46 -0500

    Update urban_routes_main_page.py

--- 3. Creating a POM File/urban_routes_main_page.py
+++ 3. Creating a POM File/urban_routes_main_page.py
@@ -1,9 +1,11 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
-# Defining the page class, locators and method in the class
 class UrbanRoutesPage:
-    # Locators as class attributes
+
+    # Locators
     FROM_LOCATOR = (By.ID, 'from')
     TO_LOCATOR = (By.ID, 'to')
     CUSTOM_OPTION_LOCATOR = (By.XPATH, '//div[text()="Custom"]')
@@ -11,24 +13,37 @@ class UrbanRoutesPage:
     BIKE_TEXT_LOCATOR = ...
 
     def __init__(self, driver):
-        self.driver = driver  # Initialize the driver
+        self.driver = driver
+        self.wait = WebDriverWait(driver, 10)
 
     def enter_from_location(self, from_text):
-        # Enter From
-        self.driver.find_element(*self.FROM_LOCATOR).send_keys(from_text)
+        from_input = self.wait.until(
+            EC.visibility_of_element_located(self.FROM_LOCATOR)
+        )
+        from_input.clear()
+        from_input.send_keys(from_text)
 
     def enter_to_location(self, to_text):
-        # Enter To
-        self.driver.find_element(*self.TO_LOCATOR).send_keys(to_text)
+        to_input = self.wait.until(
+            EC.visibility_of_element_located(self.TO_LOCATOR)
+        )
+        to_input.clear()
+        to_input.send_keys(to_text)
 
     def click_custom_option(self):
-        # Click the "Custom" option
-        ...
+        custom_button = self.wait.until(
+            EC.element_to_be_clickable(self.CUSTOM_OPTION_LOCATOR)
+        )
+        custom_button.click()
 
     def click_bike_icon(self):
-        # Click the "Bike" icon
-        ...
+        bike_icon = self.wait.until(
+            EC.element_to_be_clickable(self.BIKE_ICON_LOCATOR)
+        )
+        bike_icon.click()
 
     def get_bike_text(self):
-        # Return the "Bike" text
-        ...
+        bike_text = self.wait.until(
+            EC.visibility_of_element_located(self.BIKE_TEXT_LOCATOR)
+        )
+        return bike_text.text
